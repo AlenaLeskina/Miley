8bb1f76447c28a412bab124f5030f9d665aa06cd
AlenaLeskina <andryushhenko-1994@mail.ru>
2021-11-29 22:15:56 +0300

    Mili

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 10000;
+        int rub = 20;
+        int bonus = 1;
+        int mili = price/rub * bonus;
+        System.out.println(mili);
+    }
+}
